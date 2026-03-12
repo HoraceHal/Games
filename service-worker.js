086a19138cc2889578352035b2c128bcbbdcdbce
Horace Hal <143590603+HoraceHal@users.noreply.github.com>
2026-03-12 10:03:19 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-
-// 极简 SW：仅做跳过等待和控制，不拦截 fetch，避免缓存干扰
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-// 不添加 fetch 监听，彻底避免缓存污染
